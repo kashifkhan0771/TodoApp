57c38e14b1827885cac0e8c800181728361a2141
kashif khan <kashif.khan@wanclouds.net>
2021-02-03 12:52:20 +0500

    Domain Models Layer Added

---
+++ models/task.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"time"
+
+	"github.com/fatih/structs"
+)
+
+type Task struct {
+	ID      string                 `json:"id" structs:"id" bson:"_id" db:"id"`
+	Title   string                 `json:"title" structs:"title" bson:"title" db:"title"`
+	Desc    map[string]interface{} `json:"desc" structs:"desc" bson:"desc" db:"desc"`
+	AddedOn time.Time              `json:"added_on" structs:"added_on" bson:"added_on" db:"added_on"`
+	TodoOn  time.Time              `json:"todo_on" structs:"todo_on" bson:"todo_on" db:"todo_on"`
+	Status  string                 `json:"status" structs:"status" bson:"status" db:"status"`
+}
+
+func (t *Task) Map() map[string]interface{}{
+	return structs.Map(t)
+}
+
+// Names returns the field names of Agent model
+func (t *Task) Names() []string {
+	fields := structs.Fields(t)
+	names := make([]string, len(fields))
+
+	for i, field := range fields {
+		name := field.Name()
+		tagName := field.Tag(structs.DefaultTagName)
+		if tagName != "" {
+			name = tagName
+		}
+		names[i] = name
+	}
+
+	return names
+}
---
+++ models/task_test.go
@@ -0,0 +1,118 @@
+package models
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestTask_Map(t1 *testing.T) {
+	t1.Parallel()
+	type fields struct {
+		ID      string
+		Title   string
+		Desc    map[string]interface{}
+		AddedOn time.Time
+		TodoOn  time.Time
+		Status  string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   map[string]interface{}
+	}{
+		{
+			name : "success - Convert struct to map",
+			fields: fields{
+				ID: "1",
+				Title: "Task 1",
+				Desc: map[string]interface{}{
+					"User": "Kashif Khan",
+					"Detail": "Complete New Microservice",
+				},
+				AddedOn: time.Now().UTC().Truncate(time.Minute),
+				TodoOn: time.Date(2021, 02, 15, 12, 00, 00, 00, time.UTC),
+				Status: "Pending",
+			},
+			want: map[string]interface{}{
+				"id": "1",
+				"title": "Task 1",
+				"desc": map[string]interface{}{
+					"User": "Kashif Khan",
+					"Detail": "Complete New Microservice",
+				},
+				"added_on":time.Now().UTC().Truncate(time.Minute),
+				"todo_on": time.Date(2021, 02, 15, 12, 00, 00, 00, time.UTC),
+				"status": "Pending",
+
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t1.Parallel()
+			t := &Task{
+				ID:      tt.fields.ID,
+				Title:   tt.fields.Title,
+				Desc:    tt.fields.Desc,
+				AddedOn: tt.fields.AddedOn,
+				TodoOn:  tt.fields.TodoOn,
+				Status:  tt.fields.Status,
+			}
+			if got := t.Map(); !reflect.DeepEqual(got, tt.want) {
+				t1.Errorf("Map() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTask_Names(t1 *testing.T) {
+	t1.Parallel()
+	type fields struct {
+		ID      string
+		Title   string
+		Desc    map[string]interface{}
+		AddedOn time.Time
+		TodoOn  time.Time
+		Status  string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			name : "success - Names of task struct field",
+			fields: fields{
+				ID: "1",
+				Title: "Task 1",
+				Desc: map[string]interface{}{
+					"User": "Kashif Khan",
+					"Detail": "Complete New Microservice",
+				},
+				AddedOn: time.Now().UTC().Truncate(time.Minute),
+				TodoOn: time.Date(2021, 02, 15, 12, 00, 00, 00, time.UTC),
+				Status: "Pending",
+			},
+			want: []string{"id", "title", "desc", "added_on", "todo_on", "status"},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t1.Parallel()
+			t := &Task{
+				ID:      tt.fields.ID,
+				Title:   tt.fields.Title,
+				Desc:    tt.fields.Desc,
+				AddedOn: tt.fields.AddedOn,
+				TodoOn:  tt.fields.TodoOn,
+				Status:  tt.fields.Status,
+			}
+			if got := t.Names(); !reflect.DeepEqual(got, tt.want) {
+				t1.Errorf("Names() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
